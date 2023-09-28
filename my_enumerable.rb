4d33a2a0c96a3eb60982426f44fe77dc21df87e6
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-09-28 11:18:40 +0500

    Add max and min methods

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,39 +1,65 @@
 module MyEnumerable
   def all?
     if block_given?
-      each do |elem|
-        return false unless yield(elem)
-      end
+      each { | item | return false unless yield(item) }
     else
-      each do |elem|
-        return false unless elem
-      end
+      each { | item | return false unless item } 
     end
     true
   end
 
   def any?
     if block_given?
-      each do |elem|
-        return true if yield(elem)
-      end
+      each { | item | return true if yield(item) }
     else
-      each do |elem|
-        return true if elem
-      end
+      each { | item | return true if item } 
     end
     false
   end
 
   def filter
-    new_list = []
+    filtered_list = []
     if block_given?
-      each do |elem|
-        new_list.push(elem) if yield(elem)
-      end
-      new_list
+      each { | item | filtered_list.push(item) if yield(item) }
+      filtered_list
     else
       filter
     end
+  end
+
+  def max(num = nil)
+    if num
+      max_list = []
+      init_list = @list
+      num.times do
+        max = first
+        index = 0
+        init_list.each_with_index do |elem, i|
+          if (elem <=> max) == 1
+            max = elem
+            index = i
+          end
+        end
+        max_list.push(max)
+        init_list.delete_at(index)
+      end
+      max_list
+    else
+      max = first
+      each do |elem|
+        elem.downcase if elem.is_a? String
+        max = elem if (elem <=> max) == 1
+      end
+      max
+    end
+  end
+  
+  def min
+    min = first
+    each do |elem|
+      elem.downcase if elem.is_a? String
+      min = elem if (elem <=> min) == -1
+    end
+    min
   end
 end
--- my_list.rb
+++ my_list.rb
@@ -11,15 +11,27 @@ class MyList
   end
 end
 
-list = MyList.new(1, 2, 3, 4)
+list = MyList.new(1, 2, 5, 3, 4, 6)
+puts list.inspect
+# puts list.max
+# puts list.min
+# puts list.max(3)
 
-puts(list.all? { |e| e < 5 })
-puts(list.all? { |e| e > 5 })
-puts list.all?
-puts [0, 2, 3, nil].all?
+# puts list.min(2)
+# list1 = MyList.new('albatross', 'dog', 'horse')
+# puts list1.inspect
+# puts list1.max
+# puts list1.min
+# puts list1.max(2)
+# puts list1.min(2)
+# puts list1.max { |a, b| a.length <=> b.length }
+# puts(list.all? { |e| e < 5 })
+# puts(list.all? { |e| e > 5 })
+# puts list.all?
+# puts [0, 2, 3, nil].all?
 
 puts(list.any? { |e| e == 2 })
-puts(list.any? { |e| e == 5 })
+puts(list.any? { |e| e == 7 })
 puts list.any?
 puts [false, nil].any?
 
@@ -31,6 +43,7 @@ puts(list.filter(&:even?))
 # puts [1, 2i, 3.14].all?(Numeric)
 # puts [nil, true, 99].all?
 # puts [].all?
+# puts %w[ant bear cat].all?(/a/)
 
 # puts %w[ant bear cat].any? { |word| word.length >= 3 }
 # puts %w[ant bear cat].any? { |word| word.length >= 4 }
@@ -39,13 +52,15 @@ puts(list.filter(&:even?))
 # puts [nil, true, 99].any?
 # puts [].any?
 
-# puts (1..10).filter { |i|  i % 3 == 0 }
-# #=> [3, 6, 9]
+print (1..10).filter { |i|  i % 3 == 0 }
+#=> [3, 6, 9]
 
-# puts [1,2,3,4,5].filter { |num|  num.even?  }
-# #=> [2, 4]
+print [1,2,3,4,5].filter { |num|  num.even?  }
+#=> [2, 4]
 
-# puts [:foo, :bar].filter { |x| x == :foo }
-# #=> [:foo]
+print [:foo, :bar].filter { |x| x == :foo }
+#=> [:foo]
 
-# puts [:foo, :bar].filter.inspect
+print [:foo, :bar].filter.inspect
+
+print [].filter.inspect
